806cd7392e5239caaa6b1cb3b46c86954bf6c9ff
AlexandreXYZ <costaalexandre45@gmail.com>
2022-01-16 22:22:28 -0300

    perf(posts): add postProfile (construction) and add performace in Thumbnail
    Add placeholder Blur and quality = 90% to thumbnail Image

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -3,6 +3,7 @@ import Image from "next/image"
 import styles from './style.module.scss'
 import { Header } from "../../components/header"
 import { data } from "./models/data"
+import { PostProfile } from "../../components/postProfile"
 
 export const Post = () => {
   return(
@@ -17,7 +18,10 @@ export const Post = () => {
           <article className={styles.container_main_article}>
             <h1>{data.title}</h1>
             <p className={styles.container_main_article_resume} >{data.resume}</p>
-            <div className={styles.container_main_article_thumb} ><Image src={data.thumbnail} alt="test"/></div>
+            <PostProfile author={{name: 'Alexandre', photo: data.thumbnail}} post_created={'1/11/2222'}></PostProfile>
+            <div className={styles.container_main_article_thumb}>
+              <Image src={data.thumbnail} quality={90} placeholder='blur'/>
+            </div>
             <h2>{data.content.paragraph_1.title}</h2>
             {data.content.paragraph_1.text.map((element, i)=>{return (<p key={i}>{element}</p>)})}
             <h2>{data.content.paragraph_2.title}</h2>
