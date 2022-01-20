d8f0693f031f1a8bc0c30b4dabb1c0fc74fb8ce1
AlexandreXYZ <costaalexandre45@gmail.com>
2022-01-19 21:04:32 -0300

    fix(header): transform header props "fixed" in "Sticky" (and fix logic this)

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,15 @@
 import styles from './style.module.scss'
 import styleUtils from '../../styles/utils.module.scss'
+import Link from 'next/link';
 
 interface props {
   className?: string,
-  fixed?: boolean
+  sticky?: boolean
 }
 
-export const Header = ({ className = '', fixed }: props) => {
-  const isFixed = fixed ? styleUtils.fixed : '';
-  console.log(isFixed)
+export const Header = ({ className = '', sticky }: props) => {
+  const isFixed = sticky ? styleUtils.sticky : '';
+
   return (
     <header className={`${styles.container} ${isFixed} ${className}`}>
       <div className={styles.container_logoContainer}>
@@ -17,12 +18,12 @@ export const Header = ({ className = '', fixed }: props) => {
       </div>
       <nav className={styles.container_nav}>
         {/* Quando tiver as paginas abaixo usar a tag <Link> */}
-        <a className={styles.container_nav_topic}>Artigos</a>
+        <Link href='/'><a className={styles.container_nav_topic}>Artigos</a></Link>
         <a className={styles.container_nav_topic}>Sobre</a>
       </nav>
-      <div className={styles.container_inputContainer}>
+      {/* <div className={styles.container_inputContainer}>
         <input type="search" placeholder='Procurar' className={styles.container__search}/>
-      </div>
+      </div> */}
     </header>
   )
 } 
