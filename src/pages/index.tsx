5f2375db8044e8ed70a5bdab2727f7ed55afdf29
AlexandreXYZ <costaalexandre45@gmail.com>
2022-01-25 15:32:14 -0300

    feat(post-card): create postCard component and add initial config in home page (hub)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import type { NextPage } from 'next'
+import Head from 'next/head'
+import { Header } from '../components/header'
+import PostCard from '../components/post-card'
+import { dataExemple } from './posts/models/data'
+import styles from './style.module.scss'
 // import '../service/initFirebase'
 
 const Home: NextPage = () => {
   return (
     <>
-      {/* <Post/> */}
+      <Head>
+        <title>Self Wiser</title>
+        <meta name="description" content="Venha aprender a aprender de forma rapida e eficiênte"/>
+      </Head>
+      <Header sticky/>
+      <main>
+        <PostCard className={styles.postCard} data={dataExemple}/>
+      </main>
     </>
   )
 }
