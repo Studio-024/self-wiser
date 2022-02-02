b9c5a00d15fff195e3c7da79030411272659d62b
AlexandreXYZ <costaalexandre45@gmail.com>
2022-02-02 16:04:00 -0300

    refactor(header): apply BEM in all classNames

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -10,18 +10,16 @@ export const Header = ({ className = '', fixed }: props) => {
   const isFixed = fixed ? styleUtils : '';
 
   return (
-    <header className={`${styles.container} ${isFixed} ${className}`}>
-      <div className={styles.container_logoContainer}>
-        {/* Se for uma imagem e não um SVG, usar a tag <Image> */}
-        <img alt='Self Wiser' className={styles.container__logo}></img>
+    <header className={`${styles.header} ${isFixed} ${className}`}>
+      <div className={styles.header__logo}>
+        <img alt='Self Wiser'/>
       </div>
-      <nav className={styles.container_nav}>
-        {/* Quando tiver as paginas abaixo usar a tag <Link> */}
-        <a className={styles.container_nav_topic}>Artigos</a>
-        <a className={styles.container_nav_topic}>Sobre</a>
+      <nav className={styles.header__nav}>
+        <a className={styles.header__topic}>Artigos</a>
+        <a className={styles.header__topic}>Sobre</a>
       </nav>
-      <div className={styles.container_inputContainer}>
-        <input type="search" placeholder='Procurar' className={styles.container__search}/>
+      <div>
+        <input type="search" placeholder='Procurar' className={styles.header__search}/>
       </div>
     </header>
   )
