d671459b2e82689970e2bc24b06fcab3604ba4bb
ViniciusGregorine <viniciusgmenegon@gmail.com>
2022-02-14 15:55:30 -0300

    refactor: update auth links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,9 @@ import Link from 'next/link'
 const Home: NextPage = () => {
   return (
     <>
-      {/* <Post/> */}
       <h1>clique <Link href={'/posts/carlos-first-article'}>Nesse</Link> link</h1>
-      <h1>se <Link href={'/auth/signUp'}>registra aí</Link> tchuco</h1>
-      <h1><Link href={'/auth/signIn'}>loga aí</Link> tchuco</h1>
+      <h1>se <Link href={'/auth/sign-up'}>registra aí</Link> tchuco</h1>
+      <h1><Link href={'/auth/sign-in'}>loga aí</Link> tchuco</h1>
     </>
   )
 }
